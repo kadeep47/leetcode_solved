187a8a32842b1006f3ca85503b9e47e58b286dd7
kadeep47 <80538686+kadeep47@users.noreply.github.com>
2022-06-04 16:04:41 +0530

    Added solution - LeetHub

---
+++ Tower Of Hanoi - GFG/tower-of-hanoi.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // You need to complete this function
+
+    long long toh(int n, int from, int to, int aux) {
+        long long cnt = 1 ;
+        if(n==1){
+             cout<<"move disk 1 from rod " <<  from << " to rod " << to << endl; 
+             return 1;
+        }
+        cnt+=toh(n-1,from,aux,to);
+        cout<< "move disk " << n << " from rod " << from << " to rod "  << to << endl;
+        cnt+=toh(n-1,aux,to,from);
+        return cnt ;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;//testcases
+    while (T--) {
+        
+        int N;
+        cin >> N;//taking input N
+        
+        //calling toh() function
+        Solution ob;
+        
+        cout << ob.toh(N, 1, 3, 2) << endl;
+    }
+    return 0;
+}
+
+
+//Position this line where user code will be pasted.  // } Driver Code Ends
