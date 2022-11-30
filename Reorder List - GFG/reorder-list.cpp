59e4fb79fa174a254d89ab22393d8e6a1b9222cb
kadeep47 <80538686+kadeep47@users.noreply.github.com>
2022-11-30 21:18:38 +0530

    Added solution - LeetHub

---
+++ Reorder List - GFG/reorder-list.cpp
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Following is the Linked list node structure */
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{ 
+public:
+    void reorderList(Node* head) {
+        // Your code here
+        Node *temp = head;
+        vector<int> res;
+        while(temp){
+            res.push_back(temp->data);
+            temp=temp->next;
+        }
+        if(res.size()<=2)   return;
+        int n=res.size();
+        int i=0, idx=0;
+        vector<int> nodes(n);
+        while(idx<n){
+            nodes[idx]=res[i];
+            idx = idx+2;
+            i++;
+        }
+        idx=1, i=n-1;
+        while(idx<n){
+            nodes[idx]=res[i];
+            i--;
+            idx+=2;
+        }
+        
+        Node *order= head;
+        for(auto val: nodes){
+            order->data=val;
+            order=order->next;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+/* Function to create a new Node with given data */
+struct Node* newNode(int data) {
+    struct Node* new_Node = new Node(data);
+    new_Node->data = data;
+    new_Node->next = NULL;
+    return new_Node;
+}
+
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(void) {
+    int t, n, m, i, x;
+
+    cin >> t;
+    while (t--) {
+        struct Node* temp, *head;
+        cin >> n;
+        cin >> x;
+        head = new Node(x);
+        temp = head;
+        for (i = 0; i < n - 1; i++) {
+            cin >> x;
+            temp->next = new Node(x);
+            temp = temp->next;
+        }
+        
+        Solution ob;
+        
+        ob.reorderList(head);
+        printList(head);
+        freeList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
