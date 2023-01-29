f09e24c4b57c05ee1bd4dd04b58c81e27a1bc5ad
kadeep47 <80538686+kadeep47@users.noreply.github.com>
2023-01-29 11:54:04 +0530

    Added solution - LeetHub

---
+++ Min operations - GFG/min-operations.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int solve(int a, int b) {
+        // code here
+        int res = 0;
+
+        while (a != b) {
+
+            if (a > b) swap(a, b);
+
+            b = b & a;
+
+            res++;
+
+        }
+
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.solve(a, b);
+        
+        printf("%d\n", res);
+        
+    }
+}
+
+// } Driver Code Ends
